5917dc93e9759039b61eb16c6ed6dd012aaabd9b
Bernardo Espinoza <bernardo@discoveryspecialists.com>
2023-11-16 11:31:44 -0600

    customer_fields

--- lib/intacct.rb
+++ lib/intacct.rb
@@ -8,6 +8,12 @@ require "intacct/customer"
 require "intacct/vendor"
 require "intacct/invoice"
 require "intacct/bill"
+require "intacct/new_base"
+require "intacct/new_bill"
+require "intacct/new_customer"
+require "intacct/new_vendor"
+require "intacct/api"
+require "intacct/response"
 
 class Object
   def blank?
@@ -23,10 +29,11 @@ module Intacct
   extend self
 
   attr_accessor :xml_sender_id  , :xml_password    ,
-                :app_user_id    , :app_company_id  , :app_password ,
-                :invoice_prefix , :bill_prefix     ,
-                :vendor_prefix  , :customer_prefix, :system_name,
-                :service_url
+                :app_user_id    , :app_company_id  ,
+                :app_password   , :invoice_prefix  ,
+                :bill_prefix    , :vendor_prefix   ,
+                :customer_prefix, :system_name     ,
+                :service_url    , :customer_fields
 
   def setup
     yield self
