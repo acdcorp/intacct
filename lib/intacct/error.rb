372566b18864fc27029d2e5a25116bff3536f90d
Bernardo Espinoza <bernardo@discoveryspecialists.com>
2025-12-11 09:19:54 -0600

    rename traverse to each

--- lib/intacct/error.rb
+++ lib/intacct/error.rb
@@ -4,7 +4,7 @@ class Intacct::Error < StandardError
   def initialize(message: 'Generic instant error', sent_xml: nil, response: [])
     error_description = nil
     @sent_xml = Nokogiri::XML(sent_xml)
-    response.traverse do |n|
+    response.each do |n|
       error_description = n.content if n.name == 'description'
     end
     @response = unless error_description.blank?
